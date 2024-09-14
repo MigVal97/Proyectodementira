fa08fe13cbbb5f40b855f8110fbf6f5881fa74fa
Cristian López E <ingecris22@gmail.com>
2024-09-14 13:41:10 -0500

    Se agregó una nueva app

--- ListaProductos/JS/ListaProductos.js
+++ ListaProductos/JS/ListaProductos.js
@@ -1,10 +1,10 @@
-function addItem(item){
+function addItem(item) {
     const itemHTML = '<div class="card" style="background-color: #a5d7e8;width: 18rem;">\n' +
-        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+        '    <img src="' + item.img + '" class="card-img-top" alt="image">\n' +
         '    <div class="card-body">\n' +
-        '        <h5 class="card-title" style="font-size: 30px;">'+item.name+'</h5>\n' +
-        '        <p class="card-text" style="font-size: 20px;">'+item.description+'</p>\n' +
-        '        <p class="card-text" style="font-size: 15px;">'+item.price+'</p>\n' +
+        '        <h5 class="card-title" style="font-size: 30px;">' + item.name + '</h5>\n' +
+        '        <p class="card-text" style="font-size: 20px;">' + item.description + '</p>\n' +
+        '        <p class="card-text" style="font-size: 15px;">' + item.price + '</p>\n' +
         '        <a href="#" class="btn btn-primary" style="background-color: #576cbc; border-color: #576cbc;">Agregar al carrito</a>\n' +
         '    </div>\n' +
         '</div>\n' +
@@ -14,12 +14,23 @@ function addItem(item){
 }
 
 
-addItem({'name':'SportWatch',
-    'img':'https://static.vecteezy.com/system/resources/previews/017/494/093/non_2x/letter-s-fitness-gym-logo-design-fitness-club-exercising-logo-free-vector.jpg',
-    'description':'Registra datos relevantes para su condición física, como pasos dados, horas de sueño, frecuencia cardíaca y calorías quemadas',
-    'price' : '$20 mensual'});
+addItem({
+    'name': 'SportWatch',
+    'img': 'https://static.vecteezy.com/system/resources/previews/017/494/093/non_2x/letter-s-fitness-gym-logo-design-fitness-club-exercising-logo-free-vector.jpg',
+    'description': 'Registra datos relevantes para su condición física, como pasos dados, horas de sueño, frecuencia cardíaca y calorías quemadas',
+    'price': '$20 mensual'
+});
 
-addItem({'name':'Membership',
-    'img':'https://media.istockphoto.com/id/522389088/vector/vip-club-cards.jpg?s=612x612&w=0&k=20&c=Ms6bRFXe4iISeMBYdioSkEn-kMTW_M1cbCNg7JPSAcc=',
-    'description':'Contenido premium que sea informativo . Esto puede ser en forma de artículos, seminarios web, podcasts o recursos descargables.',
-    'price' : '$20 mensual - $200 anual'});
+addItem({
+    'name': 'Membership',
+    'img': 'https://media.istockphoto.com/id/522389088/vector/vip-club-cards.jpg?s=612x612&w=0&k=20&c=Ms6bRFXe4iISeMBYdioSkEn-kMTW_M1cbCNg7JPSAcc=',
+    'description': 'Contenido premium que sea informativo . Esto puede ser en forma de artículos, seminarios web, podcasts o recursos descargables.',
+    'price': '$20 mensual - $200 anual'
+});
+
+addItem({
+    'name': 'ReciclApp',
+    'img': 'https://www.raeeandalucia.es/sites/default/files/images/signo-reciclaje-fondo-mundo.jpg',
+    'description': 'Aplicación disponible para sistema iOs, que te permite poner a la venta tu material reciclable al por mayor o por menor...! La idea es cuidar al planeta y a la vez recibir un pago por esto...!',
+    'price': 'Suscripción $20.00 MXN'
+});    
